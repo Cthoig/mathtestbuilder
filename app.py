1e0219ba171c35d56809c48f84740d74ee17c21e
Cthoig <joshdknight99@hotmail.com>
2026-03-23 13:40:26 +1100

    Update app.py

--- app.py
+++ app.py
@@ -606,5 +606,5 @@ def download(filename):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
